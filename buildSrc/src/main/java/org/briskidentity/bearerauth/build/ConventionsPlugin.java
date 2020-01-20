76d2cc7ed5cd8c0660ae8a865a260833509f93ff
Vedran Pavic <vedran.pavic@gmail.com>
2020-01-20 20:17:08 +0100

    Configure publishing

--- buildSrc/src/main/java/org/briskidentity/bearerauth/build/ConventionsPlugin.java
+++ buildSrc/src/main/java/org/briskidentity/bearerauth/build/ConventionsPlugin.java
@@ -5,6 +5,10 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.maven.MavenPom;
+import org.gradle.api.publish.maven.MavenPublication;
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 import org.gradle.api.tasks.testing.Test;
 
 public class ConventionsPlugin implements Plugin<Project> {
@@ -12,6 +16,7 @@ public class ConventionsPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         applyJavaConventions(project);
+        applyMavenPublishConventions(project);
     }
 
     private void applyJavaConventions(Project project) {
@@ -23,6 +28,24 @@ public class ConventionsPlugin implements Plugin<Project> {
                 test.setMaxHeapSize("1g");
             });
         });
+    }
+
+    private void applyMavenPublishConventions(Project project) {
+        project.getPlugins().withType(MavenPublishPlugin.class, mavenPublishPlugin -> {
+            PublishingExtension publishingExtension = project.getExtensions().getByType(PublishingExtension.class);
+            MavenPublication mavenPublication = publishingExtension.getPublications().create("maven",
+                    MavenPublication.class);
+            MavenPom pom = mavenPublication.getPom();
+            // TODO customize POM
+            project.getPlugins().withType(JavaPlugin.class, javaPlugin -> {
+                project.getComponents().matching((component) -> component.getName().equals("java"))
+                        .all(mavenPublication::from);
+                JavaPluginExtension javaPluginExtension = project.getExtensions().getByType(JavaPluginExtension.class);
+                javaPluginExtension.withJavadocJar();
+                javaPluginExtension.withSourcesJar();
+            });
+        });
+
     }
 
 }
