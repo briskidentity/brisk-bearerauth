6c06b5e1a3b4feb6d71caf1f54a5290b2c00e63c
Vedran Pavic <vedran.pavic@gmail.com>
2020-03-28 20:40:02 +0100

    Improve build conventions

--- buildSrc/src/main/java/org/briskidentity/bearerauth/build/ConventionsPlugin.java
+++ buildSrc/src/main/java/org/briskidentity/bearerauth/build/ConventionsPlugin.java
@@ -1,5 +1,7 @@
 package org.briskidentity.bearerauth.build;
 
+import com.github.benmanes.gradle.versions.VersionsPlugin;
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask;
 import org.gradle.api.JavaVersion;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
@@ -9,10 +11,13 @@ import org.gradle.api.publish.PublishingExtension;
 import org.gradle.api.publish.maven.MavenPom;
 import org.gradle.api.publish.maven.MavenPublication;
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
+import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.testing.Test;
 
 import javax.annotation.Nonnull;
 import java.io.File;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 public class ConventionsPlugin implements Plugin<Project> {
@@ -28,11 +33,25 @@ public class ConventionsPlugin implements Plugin<Project> {
             JavaPluginExtension javaPluginExtension = project.getExtensions().getByType(JavaPluginExtension.class);
             javaPluginExtension.setSourceCompatibility(JavaVersion.VERSION_1_8);
             project.apply(action -> action.from(new File(project.getRootDir(), "gradle/dependency-versions.gradle")));
-            project.getTasks().withType(Test.class, test -> {
+            project.getPluginManager().apply(VersionsPlugin.class);
+            TaskContainer tasks = project.getTasks();
+            tasks.withType(DependencyUpdatesTask.class, dependencyUpdatesTask -> {
+                dependencyUpdatesTask.setGradleReleaseChannel("current");
+                dependencyUpdatesTask.rejectVersionIf(candidate -> isNonStable(candidate.getCandidate().getVersion())
+                        && !isNonStable(candidate.getCurrentVersion()));
+            });
+            tasks.withType(Test.class, test -> {
                 test.useJUnitPlatform();
                 test.setMaxHeapSize("1g");
             });
         });
+    }
+
+    private static boolean isNonStable(String version) {
+        boolean containsStableKeyword = Stream.of("RELEASE", "FINAL", "GA")
+                .anyMatch(s -> version.toUpperCase().contains(s));
+        boolean isStableVersion = Pattern.compile("^[0-9,.v-]+(-r)?$").matcher(version).matches();
+        return !containsStableKeyword && !isStableVersion;
     }
 
     @SuppressWarnings("UnstableApiUsage")
